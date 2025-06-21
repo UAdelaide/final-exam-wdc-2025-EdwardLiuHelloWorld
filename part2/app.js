51e465af0a019648d221eb3336f1d60c1bb47a2e
Kunxin Liu <141000582+EdwardLiuHelloWorld@users.noreply.github.com>
2025-06-21 00:59:28 +0000

    [automatic commit] 2025-06-21 00:59:28 UTC 20.227.141.210

--- part2/app.js
+++ part2/app.js
@@ -10,6 +10,11 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '/public')));
+app.use(session({
+  secret: 'yourSecretKey',
+  resave: false,
+  saveUninitialized: true
+}));
 
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
